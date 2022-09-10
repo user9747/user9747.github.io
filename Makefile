e971db8957ce87a8b0e24af99ec92f31e5fc64d1
user9747 <padinju@gmail.com>
2022-09-10 19:38:09 +0530

    feat: theming + commands

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+scss:
+	npx --yes sass --watch style.scss:style.css --style compressed --no-source-map
+
 start:
-	npx sass --watch style.scss:style.css --style compressed --no-source-map
+	npx --yes serve
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var bgText = document.getElementById("bg-text");
 var bgValue = "About";
+var typedText = ["", "", "", "", ""];
+var mode = "visual";
 
 const targets = document.querySelectorAll(".container");
 
@@ -44,4 +46,57 @@ const setBackground = (val) => {
   if (innerHTML === bgValue) return;
   bgValue = innerHTML;
   bgText.innerHTML = innerHTML;
+};
+
+const setTypedText = (key) => {
+  typedText.push(key);
+  typedText.shift();
+};
+
+const setCommandText = (text) => {
+  let commandText = document.getElementById("command-text");
+  commandText.innerHTML = "/" + typedText.join("");
+};
+
+const setModeVisual = () => {
+  mode = "visual";
+  document.getElementById("command").style.display = "none";
+  typedText = ["", "", "", "", ""];
+};
+
+const setTheme = () => {
+  if (typedText.join("").includes("lumos")) {
+    document.documentElement.setAttribute("data-theme", "light");
+    setModeVisual();
+    return;
+  }
+
+  if (typedText.join("").includes("nox")) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    setModeVisual();
+    return;
+  }
+};
+window.onkeydown = (e) => {
+  console.log(e.key);
+  if (mode === "command" && e.key === "Escape") {
+    setModeVisual();
+  }
+  if (e.key.length != 1) {
+    return;
+  }
+
+  if (mode !== "command" && e.key === "/") {
+    mode = "command";
+    document.getElementById("command").style.display = "flex";
+    setCommandText();
+    return;
+  }
+  if (mode !== "command") {
+    return;
+  }
+  setTypedText(e.key);
+  setCommandText();
+  setTheme();
+  console.log(typedText.join(""));
 };
